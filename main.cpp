400a9d65a5d441edcabe9a66191b75304d425e1d
hiroki-39 <k024g0028@g.neec.ac.jp>
2025-04-16 14:37:52 +0900

    00_03の内容

--- main.cpp
+++ main.cpp
@@ -1,11 +1,91 @@
 #include<Windows.h>
+#include<cstdint>
 
-int WINAPI WinMain(HINSTANCE,HINSTANCE,LPSTR,int)
+//ウィンドウプロシージャ
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 {
-	//出力ウィンドウへの文字出力
-	OutputDebugStringA("Hello, DirectX!\n");
+	//メッセージに応じてゲーム固有の処理を行う
+	switch (msg)
+	{
+		//ウィンドウが破棄された
+	case WM_DESTROY:
+		//OSに対して、アプリの終了を伝える
+		PostQuitMessage(0);
+		return 0;
+	}
 
-	//あ
+	//標準のメッセージ処理を行う
+	return DefWindowProc(hwnd, msg, wparam, lparam);
+}
+
+
+
+//windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+	/*---ウィンドウクラスの登録---*/
+	WNDCLASS wc{};
+	//ウィンドウプロシージャ
+	wc.lpfnWndProc = WindowProc;
+	//ウィンドウクラス名
+	wc.lpszClassName = L"CG2MyWindowClass";
+	//インスタンスハンドル
+	wc.hInstance = GetModuleHandle(nullptr);
+	//カーソル
+	wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+
+	//ウィンドウクラスの登録
+	RegisterClass(&wc);
+
+	//クライアント領域のサイズ
+	const int32_t kClientWidth = 1280;
+	const int32_t kClientHeight = 720;
+
+	//ウィンドサイズを表す構造体にクライアント領域のサイズを入れる
+	RECT wrc = { 0, 0, kClientWidth, kClientHeight };
+
+	//クライアント領域のサイズをウィンドウサイズに変換する
+	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
+
+	//ウィンドウの生成
+	HWND hwnd = CreateWindow(
+		wc.lpszClassName,     //クラス名
+		L"CG2",       //タイトルバー名
+		WS_OVERLAPPEDWINDOW,  //ウィンドウスタイル
+		CW_USEDEFAULT,        //表示座X標(Windowsに任せる)
+		CW_USEDEFAULT,        //表示Y座標(WindowsOSに任せる)
+		wrc.right - wrc.left, //ウィンドウ横幅
+		wrc.bottom - wrc.top, //ウィンドウ縦幅
+		nullptr,              //親ウィンドウハンドル
+		nullptr,              //メニューハンドル
+		wc.hInstance,         //インスタンスハンドル
+		nullptr               //オプション
+	);
+
+	//ウィンドウの表示
+	ShowWindow(hwnd, SW_SHOW);
+
+	
+
+	MSG msg{};
+
+	//ウィンドウのxボタンが押されるまでループ
+	while (msg.message != WM_QUIT)
+	{
+		//windowsにメッセージが来たら最優先で処理する
+		if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		else
+		{
+			//ゲームの更新処理
+			//ゲームの描画処理
+		}
+	}
 
 	return 0;
 }
+
+
