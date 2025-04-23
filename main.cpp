b08ef0b865a6cff9bf724fa4db1777d8cee1b900
hiroki-39 <k024g0028@g.neec.ac.jp>
2025-04-23 16:57:56 +0900

    エラーの表示の追加

--- main.cpp
+++ main.cpp
@@ -95,7 +95,23 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	Log(logStream, "Hello DirectX!\n");
 	Log(logStream, ConvertString(std::format(L"clientSize:{},{}\n", kClientWidth, kClientHeight)));
 
-	/*--- DXGIファクトリーの生成 ---*/
+#ifdef _DEBUG
+
+	ID3D12Debug1* debugController = nullptr;
+	if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&debugController))))
+	{
+		//デバックレイヤーの有効化
+		debugController->EnableDebugLayer();
+		//GPU側でもチェックを行うようにする
+		debugController->SetEnableGPUBasedValidation(TRUE);
+	}
+
+#endif 
+
+
+	/*--- DirectXの初期化 ---*/
+
+	// DXGIファクトリーの生成
 	IDXGIFactory7* dxgiFactory = nullptr;
 
 	/*HRESULTはWindows系のエラーコードであり
@@ -106,7 +122,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	どうにも出来ない場合が多いのでassertにしておく*/
 	assert(SUCCEEDED(hr));
 
-	/*--- 使用するアダプタを決定 ---*/
 	//使用するアダプタ用の変数。
 	IDXGIAdapter4* useAdapter = nullptr;
 	//良い順にアダプタを頼む
@@ -170,6 +185,47 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	assert(device != nullptr);
 	//初期化完了のメッセージを出力
 	Log(logStream, "Complete create D3D12Device!!!!\n");
+
+
+#ifdef _DEBUG
+
+	ID3D12InfoQueue* infoQueue = nullptr;
+	if (SUCCEEDED(device->QueryInterface(IID_PPV_ARGS(&infoQueue))))
+	{
+		//ヤバイエラー時に止まる
+		infoQueue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_CORRUPTION, true);
+		//エラー時に止まる
+		infoQueue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_ERROR, true);
+		//警告時に止まる
+		infoQueue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_WARNING, true);
+		//抑制するメッセージのID
+		D3D12_MESSAGE_ID denyIds[] =
+		{
+			//デバッグレイヤーの警告
+			D3D12_MESSAGE_ID_RESOURCE_BARRIER_MISMATCHING_COMMAND_LIST_TYPE,
+		};
+
+		//抑制するレベル
+		D3D12_MESSAGE_SEVERITY severities[] =
+		{
+			//警告
+			D3D12_MESSAGE_SEVERITY_INFO
+		};
+
+		D3D12_INFO_QUEUE_FILTER filter{};
+		filter.DenyList.NumIDs = _countof(denyIds);
+		filter.DenyList.pIDList = denyIds;
+		filter.DenyList.NumSeverities = _countof(severities);
+		filter.DenyList.pSeverityList = severities;
+
+		//指定したメッセージの表示を抑制
+		infoQueue->AddStorageFilterEntries(&filter);
+
+		//解放
+		infoQueue->Release();
+	}
+
+#endif 
 
 	//コマンドキューの生成
 	ID3D12CommandQueue* commandQueue = nullptr;
@@ -294,8 +350,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		{
 			TranslateMessage(&msg);
 			DispatchMessage(&msg);
-		} 
-		else
+		} else
 		{
 			//ゲームの更新処理
 
