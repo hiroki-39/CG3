e98def75a1432aaa7d514a2e8607223b36c9ba66
hiroki-39 <k024g0028@g.neec.ac.jp>
2025-06-11 11:31:52 +0900

    法線情報の追加

--- main.cpp
+++ main.cpp
@@ -46,6 +46,7 @@ struct VertexData
 
 	Vector4 position;
 	Vector2 texcoord;
+	Vector3 normal;
 };
 
 
@@ -513,7 +514,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	assert(SUCCEEDED(hr));
 
 	//inputLayout
-	D3D12_INPUT_ELEMENT_DESC inputElementDescs[2] = {};
+	D3D12_INPUT_ELEMENT_DESC inputElementDescs[3] = {};
 	inputElementDescs[0].SemanticName = "POSITION";
 	inputElementDescs[0].SemanticIndex = 0;
 	inputElementDescs[0].Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
@@ -523,6 +524,11 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	inputElementDescs[1].SemanticIndex = 0;
 	inputElementDescs[1].Format = DXGI_FORMAT_R32G32_FLOAT;
 	inputElementDescs[1].AlignedByteOffset = D3D12_APPEND_ALIGNED_ELEMENT;
+
+	inputElementDescs[2].SemanticName = "NORMAL";
+	inputElementDescs[2].SemanticIndex = 0;
+	inputElementDescs[2].Format = DXGI_FORMAT_R32G32B32_FLOAT;
+	inputElementDescs[2].AlignedByteOffset = D3D12_APPEND_ALIGNED_ELEMENT;
 
 	D3D12_INPUT_LAYOUT_DESC inputLayoutDesc{};
 	inputLayoutDesc.pInputElementDescs = inputElementDescs;
@@ -722,11 +728,15 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 			vertexData[start + 0].position.y = sin(lat);
 			vertexData[start + 0].position.z = cos(lat) * sin(lon);
 			vertexData[start + 0].position.w = 1.0f;
+
 			vertexData[start + 0].texcoord = {
 				float(lonIndex) / float(kSubdivision),
 				 1.0f - float(latIndex) / float(kSubdivision)
 			};
 
+			vertexData[start + 0].normal.x = vertexData[start + 0].position.x;
+			vertexData[start + 0].normal.x = vertexData[start + 0].position.y;
+			vertexData[start + 0].normal.x = vertexData[start + 0].position.z;
 
 
 			//b
@@ -734,22 +744,30 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 			vertexData[start + 1].position.y = sin(lat + kLatEvery);
 			vertexData[start + 1].position.z = cos(lat + kLatEvery) * sin(lon);
 			vertexData[start + 1].position.w = 1.0f;
+
 			vertexData[start + 1].texcoord = {
 				float(lonIndex) / float(kSubdivision),
 				 1.0f - float(latIndex + 1) / float(kSubdivision)
 			};
 
-
+			vertexData[start + 1].normal.x = vertexData[start + 1].position.x;
+			vertexData[start + 1].normal.x = vertexData[start + 1].position.y;
+			vertexData[start + 1].normal.x = vertexData[start + 1].position.z;
 
 			//c
 			vertexData[start + 2].position.x = cos(lat) * cos(lon + kLonEvery);
 			vertexData[start + 2].position.y = sin(lat);
 			vertexData[start + 2].position.z = cos(lat) * sin(lon + kLonEvery);
 			vertexData[start + 2].position.w = 1.0f;
+
 			vertexData[start + 2].texcoord = {
 				float(lonIndex + 1) / float(kSubdivision),
 				 1.0f - float(latIndex) / float(kSubdivision)
 			};
+
+			vertexData[start + 2].normal.x = vertexData[start + 2].position.x;
+			vertexData[start + 2].normal.x = vertexData[start + 2].position.y;
+			vertexData[start + 2].normal.x = vertexData[start + 2].position.z;
 
 			//三角形2つ目: c,b,d
 
@@ -758,25 +776,41 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 			vertexData[start + 3].position.y = vertexData[start + 2].position.y;
 			vertexData[start + 3].position.z = vertexData[start + 2].position.z;
 			vertexData[start + 3].position.w = vertexData[start + 2].position.w;
+
 			vertexData[start + 3].texcoord = vertexData[start + 2].texcoord;
+
+			vertexData[start + 3].normal.x = vertexData[start + 3].position.x;
+			vertexData[start + 3].normal.x = vertexData[start + 3].position.y;
+			vertexData[start + 3].normal.x = vertexData[start + 3].position.z;
+
 
 			// b
 			vertexData[start + 4].position.x = vertexData[start + 1].position.x;
 			vertexData[start + 4].position.y = vertexData[start + 1].position.y;
 			vertexData[start + 4].position.z = vertexData[start + 1].position.z;
 			vertexData[start + 4].position.w = vertexData[start + 1].position.w;
+
 			vertexData[start + 4].texcoord = vertexData[start + 1].texcoord;
+
+			vertexData[start + 4].normal.x = vertexData[start + 4].position.x;
+			vertexData[start + 4].normal.x = vertexData[start + 4].position.y;
+			vertexData[start + 4].normal.x = vertexData[start + 4].position.z;
+
 
 			//d
 			vertexData[start + 5].position.x = cos(lat + kLatEvery) * cos(lon + kLonEvery);
 			vertexData[start + 5].position.y = sin(lat + kLatEvery);
 			vertexData[start + 5].position.z = cos(lat + kLatEvery) * sin(lon + kLonEvery);
 			vertexData[start + 5].position.w = 1.0f;
+
 			vertexData[start + 5].texcoord = {
 				float(lonIndex + 1) / float(kSubdivision),
 				 1.0f - float(latIndex + 1) / float(kSubdivision)
 			};
 
+			vertexData[start + 5].normal.x = vertexData[start + 5].position.x;
+			vertexData[start + 5].normal.x = vertexData[start + 5].position.y;
+			vertexData[start + 5].normal.x = vertexData[start + 5].position.z;
 
 		}
 	}
@@ -815,27 +849,33 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	//左下
 	vertexDataSprite[0].position = { 0.0f,360.0f,0.0f,1.0f };
 	vertexDataSprite[0].texcoord = { 0.0f,1.0f };
+	vertexDataSprite[0].normal = { 0.0f,0.0f,-1.0f };
 
 	//左上
 	vertexDataSprite[1].position = { 0.0f,0.0f,0.0f,1.0f };
 	vertexDataSprite[1].texcoord = { 0.0f,0.0f };
+	vertexDataSprite[1].normal = { 0.0f,0.0f,-1.0f };
 
 	//右下
 	vertexDataSprite[2].position = { 640.0f,360.0f,0.0f,1.0f };
 	vertexDataSprite[2].texcoord = { 1.0f,1.0f };
+	vertexDataSprite[2].normal = { 0.0f,0.0f,-1.0f };
 
 	//2枚目の三角形
 	//左上
 	vertexDataSprite[3].position = { 0.0f,0.0f,0.0f,1.0f, };
 	vertexDataSprite[3].texcoord = { 0.0f,0.0f };
+	vertexDataSprite[3].normal = { 0.0f,0.0f,-1.0f };
 
 	//右上
 	vertexDataSprite[4].position = { 640.0f,0.0f,0.0f,1.0f };
 	vertexDataSprite[4].texcoord = { 1.0f,0.0f };
+	vertexDataSprite[4].normal = { 0.0f,0.0f,-1.0f };
 
 	//右下
 	vertexDataSprite[5].position = { 640.0f,360.0f,0.0f,1.0f };
 	vertexDataSprite[5].texcoord = { 1.0f,1.0f };
+	vertexDataSprite[5].normal = { 0.0f,0.0f,-1.0f };
 
 	//Sprite用のTransformationMatrix用のリソースを作る。matrix4x4　1つ分のサイズを用意する
 	ID3D12Resource* transformationMatrixResourceSprite = CreateBufferResource(device, sizeof(Matrix4x4));
@@ -997,7 +1037,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 
 			/*--- ↓更新処理ここから↓ ---*/
 
-		
+
 
 			transform.rotate.y += 0.03f;
 
@@ -1025,7 +1065,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 
 			ImGui::Begin("window");
 			ImGui::ColorEdit4("Color", &(*materialData).x);
-			ImGui::Checkbox("useMonsterBall",&useMonsterBall);
+			ImGui::Checkbox("useMonsterBall", &useMonsterBall);
 			ImGui::End();
 
 			/*--- ↓描画処理ここから↓ ---*/
@@ -1105,16 +1145,16 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 			commandList->DrawInstanced(vertexCount, 1, 0, 0);
 
 			/*--- スプライト ---*/
-			
+
 			//VBVの設定
 			commandList->IASetVertexBuffers(0, 1, &vertexBufferviewSprite);
-			
+
 			//TransfomationMatrixCBufferの場所を指定
 			commandList->SetGraphicsRootConstantBufferView(1, transformationMatrixResourceSprite->GetGPUVirtualAddress());
-			
+
 			//SRVのDescriptorTableの先頭を設定
 			commandList->SetGraphicsRootDescriptorTable(2, textureSrvHandleGPU1);
-			
+
 			//描画！
 			commandList->DrawInstanced(6, 1, 0, 0);
 
