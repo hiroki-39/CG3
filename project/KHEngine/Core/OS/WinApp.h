683c7db54e2370050149685d3b6bc7cefc14e2cc
hiroki kato <k024g0028@g.neec.ac.jp>
2026-03-03 22:30:52 +0900

    設定を変更

--- project/KHEngine/Core/OS/WinApp.h
+++ project/KHEngine/Core/OS/WinApp.h
@@ -44,8 +44,8 @@ public:
 public://定数
 
 	//クライアント領域のサイズ
-	static const int32_t kClientWidth = 1280;
-	static const int32_t kClientHeight = 720;
+	static const int32_t kClientWidth = 500;
+	static const int32_t kClientHeight = 100;
 
 private:
 
