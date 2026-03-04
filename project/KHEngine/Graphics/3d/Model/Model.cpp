15c361a0109e623c3b72945773efa69b8f1bbf06
hiroki kato <k024g0028@g.neec.ac.jp>
2026-03-04 15:40:33 +0900

    imguiを見やすく変更

--- project/KHEngine/Graphics/3d/Model/Model.cpp
+++ project/KHEngine/Graphics/3d/Model/Model.cpp
@@ -105,7 +105,7 @@ void Model::CreateMaterialResource()
 	materialData_->enableLighting = true;
 
 	//Lightingの種類の設定
-	materialData_->selectLightings = 0;
+	materialData_->selectLightings = 3;
 
 	//単位行列を書き込む
 	materialData_->uvTransform = Matrix4x4::Identity();
